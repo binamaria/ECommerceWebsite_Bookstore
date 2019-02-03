4c6d7946c074601133a55690a2a7a304914af131
binamaria <binamoria@gmail.com>
2019-02-02 23:56:31 -0500

    Initial commit

---
+++ src/main/java/com/bookstore/service/impl/UserServiceImpl.java
@@ -0,0 +1,81 @@
+package com.bookstore.service.impl;
+
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.domain.User;
+import com.bookstore.domain.security.PasswordResetToken;
+import com.bookstore.domain.security.UserRole;
+import com.bookstore.repository.PasswordResetTokenRepository;
+import com.bookstore.repository.RoleRepository;
+import com.bookstore.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService{
+	
+	private static final Logger LOG =LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserRepository userRepository; 
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Autowired
+	private PasswordResetTokenRepository passwordResetTokenRepository;
+	
+	@Override
+	public PasswordResetToken getPasswordResetToken(final String token){
+		return passwordResetTokenRepository.findByToken(token);
+	}
+	
+	@Override
+	public void createPasswordResetTokenForUser(final User user, final String token)
+	{
+		final PasswordResetToken myToken = new PasswordResetToken(token, user);
+		passwordResetTokenRepository.save(myToken);
+	}
+
+	@Override
+	public User findByUsername(String username) {
+		// TODO Auto-generated method stub
+		
+		return userRepository.findByUsername(username);
+	}
+
+	@Override
+	public User findByEmail(String email) {
+		// TODO Auto-generated method stub
+		return userRepository.findByEmail(email);
+	}
+	@Override
+	public User createUser(User user, Set<UserRole> userRoles) throws Exception{
+		User localUser = userRepository.findByUsername(user.getUsername());
+		
+		if(localUser != null){
+			LOG.info("user {} already exists. Nothing will be done.", user.getUsername());
+		}else{
+			for(UserRole ur : userRoles)
+			{
+				roleRepository.save(ur.getRole());
+			}
+			
+			user.getUserRoles().addAll(userRoles);
+			localUser = userRepository.save(user);
+		}
+		return localUser;
+		//bina 4.35-adding createUser
+		
+	}
+
+	@Override
+	public User save(User user) {
+		// TODO Auto-generated method stub
+		return userRepository.save(user);
+	}
+
+}
